85d010919f9d37e6c83e64dae8e219388767e128
Day-Aaron-661 <aaron.cs12@nycu.edu.tw>
2025-05-19 20:08:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,110 +1,68 @@
-from CNN import AudioCNN
-#from BERT import TextEncoder
-#from Fusion import LateFusionModel
-
+import os
+import csv
 import torch
-from  torch.utils.data import DataLoader , Dataset
-from torch import nn , optim
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import pickle
 
+from lyrics_model import (
+    load_lyrics_folder, load_test_folder,
+    Vocab, LyricsDataset, LyricsTestDataset, CNNModel, train
+)
 
-#///////////////////////////////////////////////////////////////////////////#
-                          # I n i t i a l i z e
-#///////////////////////////////////////////////////////////////////////////#
+# Load training and validation data
+train_texts, train_valence, train_energy = load_lyrics_folder("lyrics")
+val_texts, val_valence, val_energy = load_lyrics_folder("validation")
 
-audio_model = AudioCNN()
-text_model = TextEncoder()
-fusion_model = LateFusionModel( audio_dim=128 , text_dim=768 , output_dim=2 )
+# Build and save vocab
+vocab = Vocab(train_texts)
+with open("vocab.pkl", "wb") as f:
+    pickle.dump(vocab, f)
+
+# Prepare datasets and dataloaders
+train_dataset = LyricsDataset(train_texts, train_valence, train_energy, vocab)
+val_dataset = LyricsDataset(val_texts, val_valence, val_energy, vocab)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=16)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-audio_model = audio_model().to(device)
-text_model = text_model().to(device)
-fusion_model = fusion_model().to(device)
+model = CNNModel(len(vocab.stoi)).to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_fn = torch.nn.MSELoss()
 
-train_dataset = MusicDataset( csv_path = 'TBD' , audio_dir = 'TBD' , lyric_path = 'TBD' , split = 'train' )
-train_loader = DataLoader( train_dataset , batch_size = 'TBD' , shuffle = True )
-
-val_dataset = MusicDataset( csv_path = 'TBD' , audio_dir = 'TBD' , lyric_path = 'TBD' , split = 'validate' )
-val_loader = DataLoader( val_dataset , batch_size = 'TBD' , shuffle = True )
-
-test_dataset = MusicDataset( csv_path = 'TBD' , audio_dir = 'TBD' , lyric_path = 'TBD' , split = 'test' )
-test_loader = DataLoader( test_dataset , batch_size = 'TBD' , shuffle = True )
-
-criterion = nn.MSELoss()
-optimizer = optim.Adam( fusion_model.parameters() , lr=1e-4 )
+# Train the model
+train(model, train_loader, val_loader, optimizer, loss_fn, device, epochs=10)
 
 
+# Load vocab and model
+with open("vocab.pkl", "rb") as f:
+    vocab = pickle.load(f)
 
-#///////////////////////////////////////////////////////////////////////////#
-                            # t r a i n i n g
-#///////////////////////////////////////////////////////////////////////////#
+model = CNNModel(len(vocab.stoi)).to(device)
+model.load_state_dict(torch.load("best_model_lyrics.pt"))
+model.eval()
 
-print( "training session" )
+# Load test lyrics
+test_data = load_test_folder("test")
+test_texts = [lyric for _, lyric in sorted(test_data, key=lambda x: x[0])]
+filenames = [filename for filename, _ in sorted(test_data, key=lambda x: x[0])]
 
-train_losses = []
-val_losses = []
+test_dataset = LyricsTestDataset(test_texts, vocab)
+results = []
 
-EPOCHS = 10
-for epoch in range(EPOCHS):
-    audio_model.train()
-    text_model.train()
-    fusion_model.train()
-    total_loss = 0
-    
-    for audios , lyrics , labels in train_loader:
-        audios , lyrics , labels = audios.to(device), lyrics.to(device), labels.to(device)
+for i in range(len(test_dataset)):
+    token = test_dataset[i].unsqueeze(0).to(device)
+    with torch.no_grad():
+        pred = model(token)
+        valence = pred[0][0].item()
+        energy = pred[0][1].item()
+    results.append([filenames[i], valence, energy])
 
-        audios_feature = audio_model(audios)
-        lyrics_feature = text_model(lyrics)
+# Write predictions to CSV
+with open("test_predictions.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["filename", "valence", "energy"])
+    writer.writerows(results)
 
-        predict_value = fusion_model( audios_feature , lyrics_feature )
-
-        train_loss = criterion( predict_value , labels )
-        optimizer.zero_grad()
-        train_loss.backward()
-        optimizer.step()
-
-        total_loss += train_loss.item()
-        train_losses.append(train_loss)
-
-    avg_loss = total_loss / len(train_loader)
-    print(f"Epoch: {epoch+1} finished - Training Loss: {avg_loss:.4f}")
-
-
-
-#///////////////////////////////////////////////////////////////////////////#
-                            # v a l i d a t e 
-#///////////////////////////////////////////////////////////////////////////#
-
-print( "validate session" )
-
-audio_model.eval()
-text_model.eval()
-fusion_model.eval()
-
-val_losses = []
-
-total_val_loss = 0
-
-with torch.no_grad():
-    for audios, lyrics, labels in val_loader:
-        audio = audio.to(device)
-        lyrics = lyrics.to(device)
-        labels = labels.to(device)
-
-        audios_feature_val = audio_model(audio)
-        lyrics_feature_val = text_model(lyrics)
-
-        predict_value_val = fusion_model(audios_feature_val, lyrics_feature_val)
-
-        val_loss = criterion(predict_value_val, labels)
-        val_losses.append(val_loss)
-        total_val_loss += val_loss.item()
-
-avg_val_loss = total_val_loss / len(val_loader)
-print(f"Epoch: {epoch+1} finished - Val Loss: {avg_val_loss:.4f}")
-
-
-
-#///////////////////////////////////////////////////////////////////////////#
-                              # t e s t i n g 
-#///////////////////////////////////////////////////////////////////////////#
+print("Prediction saved to test_predictions.csv")
