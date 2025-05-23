1547cbcc64c08beb0494fbab742f4c778019b9c3
Day-Aaron-661 <aaron.cs12@nycu.edu.tw>
2025-05-23 21:09:37 +0800

    Update audio_model.py

--- audio_model.py
+++ audio_model.py
@@ -118,19 +118,6 @@ class AudioCNN_solo(nn.Module):
         x = self.fc2(x)
         x = self.sigmoid(x)
         return x
-    
-
-
-
-    def forward ( self , x ):
-       
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc_layers(x)
-
-        return x
 
 
 def train(model: AudioCNN_solo, train_loader: DataLoader, criterion, optimizer, device) -> float:
