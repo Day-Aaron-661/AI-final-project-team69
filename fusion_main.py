18ff523e2431a0e05a429198242865f54f87253b
Day-Aaron-661 <aaron.cs12@nycu.edu.tw>
2025-05-24 16:41:49 +0800

    Update fusion_main.py

--- fusion_main.py
+++ fusion_main.py
@@ -21,6 +21,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 max_len = 2000
 batch_size = 16  # Set your desired batch size
+vocab = Vocab.load_from_txt("vocab.txt")
+# Initialize models
+audio_model = AudioCNN().to(device)
+text_model = CNNModel(len(vocab.stoi)).to(device)
+
+audio_model.load_state_dict(torch.load('best_model_audio.pt'))
+text_model.load_state_dict(torch.load('best_model_lyrics.pt'))
+
+fusion_model = FusionModel(audio_dim=128, text_dim=128, output_dim=2).to(device)
 
 #///////////////////////////////////////////////////////////////////////////#
                        # L o a d  D a t a ( train )
@@ -28,23 +37,18 @@ batch_size = 16  # Set your desired batch size
 #這部分從 dataset load data，把三種data(audio.mp3 , lyric.txt , labels)放進同一個 data_loader 中，之後把 data_loader 送進 fusion_model
 
 train_ids, train_labels = get_ids_and_labels(csv_path='data//labels.csv', Type='train')
+
 train_audios_paths = get_audios_paths(train_ids, audio_file_path='data//audio')
 train_lyrics_paths = get_lyrics_paths(train_ids, lyric_file_path='data//lyrics')
 
 train_audios = load_audio(train_audios_paths)
 train_lyrics = load_lyric(train_lyrics_paths)
 
-vocab_train = Vocab(train_lyrics)
-
-train_lyrics = [tokenize(t, vocab_train, max_len) for t in train_lyrics]  # 把 text tokenize
+train_lyrics = [tokenize(t, vocab, max_len) for t in train_lyrics]  # 把 text tokenize
 
 train_dataset = Combined_Dataset(train_audios, train_lyrics, train_labels)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-# Initialize models
-audio_model = AudioCNN().to(device)
-text_model = CNNModel(len(vocab_train.stoi)).to(device)
-fusion_model = FusionModel(audio_dim=128, text_dim=128, output_dim=2).to(device)
 
 #///////////////////////////////////////////////////////////////////////////#
                      # L o a d  D a t a ( validate )
@@ -57,7 +61,7 @@ val_lyrics_paths = get_lyrics_paths(val_ids, lyric_file_path='data//lyrics')
 val_audios = load_audio(val_audios_paths)
 val_lyrics = load_lyric(val_lyrics_paths)
 
-val_lyrics = [tokenize(t, vocab_train, max_len) for t in val_lyrics]  # 把 text tokenize
+val_lyrics = [tokenize(t, vocab, max_len) for t in val_lyrics]  # 把 text tokenize
 
 val_dataset = Combined_Dataset(val_audios, val_lyrics, val_labels)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
@@ -75,10 +79,10 @@ test_lyrics_paths = get_lyrics_paths(test_ids, lyric_file_path='data//lyrics')
 test_audios = load_audio(test_audios_paths)
 test_lyrics = load_lyric(test_lyrics_paths)
 
-test_lyrics = [tokenize(t, vocab_train, max_len) for t in test_lyrics]  # 把 text tokenize
+test_lyrics = [tokenize(t, vocab, max_len) for t in test_lyrics]  # 把 text tokenize
 
 test_dataset = Combined_Dataset(test_audios, test_lyrics, test_ids)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 #///////////////////////////////////////////////////////////////////////////#
                             # t r a i n i n g
@@ -95,15 +99,15 @@ val_losses = []
 
 EPOCHS = 10
 for epoch in range(EPOCHS):
-    audio_model.train()
-    text_model.train()
+    audio_model.eval()
+    text_model.eval()
     fusion_model.train()
     total_loss = 0
 
     for audios, lyrics, labels in train_loader:
         audios, lyrics, labels = audios.to(device), lyrics.to(device), labels.to(device)
 
-        audios_feature = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.train()
+        audios_feature, _ = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.train()
         lyrics_feature, _ = text_model(lyrics) #得到 lyric feature_vector，到時候作為 input 送給 fusion_model.train()
 
         predict_value = fusion_model(audios_feature, lyrics_feature)
@@ -140,7 +144,7 @@ with torch.no_grad():
         lyrics = lyrics.to(device)
         labels = labels.to(device)
 
-        audios_feature_val = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.validate()
+        audios_feature_val, _ = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.validate()
         lyrics_feature_val, _ = text_model(lyrics) #得到 lyric feature_vector，到時候作為 input 送給 fusion_model.validate()
 
         predict_value_val = fusion_model(audios_feature_val, lyrics_feature_val)
@@ -174,7 +178,7 @@ with torch.no_grad():
         lyrics = lyrics.to(device)
         labels = labels.to(device)
 
-        audios_feature_test = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.validate()
+        audios_feature_test, _ = audio_model(audios) #得到 audio feature_vector，到時候作為 input 送給 fusion_model.validate()
         lyrics_feature_test, _ = text_model(lyrics) #得到 lyric feature_vector，到時候作為 input 送給 fusion_model.validate()
 
         predict_value_test = fusion_model(audios_feature_test, lyrics_feature_test)
@@ -183,8 +187,8 @@ with torch.no_grad():
         # test_losses.append(test_loss.item())
         # total_test_loss += test_loss.item()
 
-        energies = predict_value_test[:, 1].tolist() 
-        valences = predict_value_test[:, 0].tolist()
+        energies = predict_value_test[:, 0].tolist() 
+        valences = predict_value_test[:, 1].tolist()
         audio_ids = audio_ids.tolist()
 
         for audio_id, energy, valence in zip(audio_ids, energies, valences):
