dcbc642070ee8a4e3ed64f20d711a65ff314c5b3
Day-Aaron-661 <aaron.cs12@nycu.edu.tw>
2025-05-24 16:41:05 +0800

    Update audio_model.py

--- audio_model.py
+++ audio_model.py
@@ -9,56 +9,10 @@ import pandas as pd
 
 
 #///////////////////////////////////////////////////////////////////////////#
-                          # A u d i o C N N
-#///////////////////////////////////////////////////////////////////////////#
-
-class AudioCNN(nn.Module):
-    def __init__(self , n_mels = 256 , fixed_frame = 1024 ):
-        super(AudioCNN, self).__init__()
-        self.n_mels = n_mels
-        self.fixed_frame = fixed_frame
-
-        self.conv1 = nn.Conv2d( in_channels=1 , out_channels=16 , kernel_size=3 , padding=1 )
-        self.pool1 = nn.MaxPool2d( kernel_size=2 )
-
-        self.conv2 = nn.Conv2d( in_channels=16 , out_channels=32 , kernel_size=3 , padding=1 )
-        self.pool2 = nn.MaxPool2d( kernel_size=2 )
-
-        self.conv3 = nn.Conv2d( in_channels=32 , out_channels=64 , kernel_size=3 , padding=1 )
-        self.pool3 = nn.MaxPool2d( kernel_size=2)
-
-        self.fc1 = nn.Linear(64 * 32 * 128 ,2048)
-        self.fc2 = nn.Linear(2048,128)
-
-        self.relu = nn.ReLU()
-
-    def forward ( self , x ):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.pool1(x)
-
-        x = self.conv2(x)
-        x = self.relu(x)
-        x = self.pool2(x)
-
-        x = self.conv3(x)
-        x = self.relu(x)
-        x = self.pool3(x)
-
-        x = x.view( x.size(0) , -1 )    
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-
-        return x
-    
-
-
-#///////////////////////////////////////////////////////////////////////////#
                           # M p 3 to mel-spectrogram 
 #///////////////////////////////////////////////////////////////////////////#
 
-def mp3_to_mel( file_path , sr=22050 , n_mels=256, fixed_frames=1024 , duration=30 ):
+def mp3_to_mel( file_path , sr=22050 , n_mels=128, fixed_frames=1024 , duration=30 ):
     y, _ = librosa.load(file_path , sr = sr , duration = duration )
     mel = librosa.feature.melspectrogram( y = y , sr = sr , n_mels = n_mels )
     mel = librosa.power_to_db(mel , ref=np.max )
@@ -75,30 +29,27 @@ def mp3_to_mel( file_path , sr=22050 , n_mels=256, fixed_frames=1024 , duration=
     return mel_tensor
 
 
-
 #///////////////////////////////////////////////////////////////////////////#
-                          # A u d i o C N N solo 
+                          # A u d i o C N N
 #///////////////////////////////////////////////////////////////////////////#
 
-
-class AudioCNN_solo(nn.Module):
-    def __init__(self , n_mels = 256 , fixed_frame = 1024 ):
-        super(AudioCNN_solo, self).__init__()
+class AudioCNN(nn.Module):
+    def __init__(self , n_mels = 128 , fixed_frame = 1024 ):
+        super(AudioCNN, self).__init__()
         self.n_mels = n_mels
         self.fixed_frame = fixed_frame
 
         self.conv1 = nn.Conv2d( in_channels=1 , out_channels=16 , kernel_size=3 , padding=1 )
-        self.pool1 = nn.MaxPool2d( kernel_size=2 ) # 16 , 16 , 128 ,512
+        self.pool1 = nn.MaxPool2d( kernel_size=2 ) # 16 , 16 , 64 ,512
 
         self.conv2 = nn.Conv2d( in_channels=16 , out_channels=32 , kernel_size=3 , padding=1 )
-        self.pool2 = nn.MaxPool2d( kernel_size=2 ) # 16 , 32 , 64 ,256
+        self.pool2 = nn.MaxPool2d( kernel_size=2 ) # 16 , 32 , 32 ,256
 
         self.conv3 = nn.Conv2d( in_channels=32 , out_channels=64 , kernel_size=3 , padding=1 )
-        self.pool3 = nn.MaxPool2d( kernel_size=2 ) # 16 , 64 , 32 , 128
+        self.pool3 = nn.MaxPool2d( kernel_size=2 ) # 16 , 64 , 16 , 128
 
-        self.fc1 = nn.Linear(64 * 32 * 128 ,2048)
-        self.fc2 = nn.Linear(2048,128)
-        self.fc3 = nn.Linear(128,2)
+        self.fc1 = nn.Linear(64 * 16 * 128 ,128)
+        self.fc2 = nn.Linear(128,2)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
@@ -116,18 +67,17 @@ class AudioCNN_solo(nn.Module):
         x = self.pool3(x)
 
         x = x.view( x.size(0) , -1 )    
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
 
-        x = self.sigmoid(x)
-        return x
+        feature = self.fc1(x) # 128
+        output = self.relu(feature) 
+
+        output = self.fc2(output)
+
+        output = self.sigmoid(output) # 2
+        return feature , output
     
 
-
-def train(model: AudioCNN_solo, train_loader: DataLoader, criterion, optimizer, device) -> float:
+def train(model: AudioCNN, train_loader: DataLoader, criterion, optimizer, device) -> float:
     model.train()
 
     total_loss = 0.0
@@ -138,7 +88,7 @@ def train(model: AudioCNN_solo, train_loader: DataLoader, criterion, optimizer, 
         audios = audios.to(device)
 
         optimizer.zero_grad()
-        outputs = model(audios)
+        _ , outputs = model(audios)
 
         loss = criterion(outputs , labels)
         
@@ -153,7 +103,7 @@ def train(model: AudioCNN_solo, train_loader: DataLoader, criterion, optimizer, 
 
 
 
-def validate(model: AudioCNN_solo, val_loader: DataLoader, criterion, device) -> float:
+def validate(model: AudioCNN, val_loader: DataLoader, criterion, device) -> float:
     model.eval()
     
     total_loss = 0.0
@@ -165,7 +115,7 @@ def validate(model: AudioCNN_solo, val_loader: DataLoader, criterion, device) ->
             labels = labels.to(device)
             audios = audios.to(device)
 
-            outputs = model(audios)
+            _ , outputs = model(audios)
 
             loss = criterion(outputs , labels)
             total_loss += loss.item() * audios.size(0)
@@ -176,7 +126,7 @@ def validate(model: AudioCNN_solo, val_loader: DataLoader, criterion, device) ->
 
 
 
-def test(model: AudioCNN_solo, test_loader: DataLoader, criterion, device):
+def test(model: AudioCNN, test_loader: DataLoader, criterion, device):
     model.eval()
     results = []
 
@@ -185,7 +135,7 @@ def test(model: AudioCNN_solo, test_loader: DataLoader, criterion, device):
     for audios , audio_ids in progress:
         audios = audios.to(device)
 
-        outputs = model(audios)
+        _ , outputs = model(audios)
 
         energies = outputs[:, 0].tolist() 
         valences = outputs[:, 1].tolist()
